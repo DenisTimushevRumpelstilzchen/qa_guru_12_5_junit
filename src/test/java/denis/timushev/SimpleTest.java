756d740ebb38d4611886a0e62eda3abee94d68cf
tdv14011984@gmail.com <Parol2022!-forGIT>
2022-04-25 21:43:21 +0300

    add @DisplayName

--- src/test/java/denis/timushev/SimpleTest.java
+++ src/test/java/denis/timushev/SimpleTest.java
@@ -4,13 +4,11 @@ import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
-@DisplayName("Класс с демонстрационными тестами")
 public class SimpleTest {
 
     @DisplayName("Демонстрационный тест")
     @Test
     void firstTest() {
-        // Вот тут проверим ...
         Assertions.assertTrue(3 > 2);
         Assertions.assertFalse(3 < 2);
         Assertions.assertEquals("Foo", "Foo");
@@ -20,4 +18,9 @@ public class SimpleTest {
         );
     }
 
+    @DisplayName("Демонстрационный тест № 2")
+    @Test
+    void secondTest() {
+        Assertions.assertTrue(3 < 2);
+    }
 }
