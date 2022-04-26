7b8e3a52fcc9d08bae92c45978a7fd74c6decdc6
Your NameDenis <tdv14011984@gmail.com>
2022-04-26 14:43:20 +0300

    end of the lesson

--- src/test/java/denis/timushev/WebTest.java
+++ src/test/java/denis/timushev/WebTest.java
@@ -2,9 +2,8 @@ package denis.timushev;
 
 import com.codeborne.selenide.Condition;
 import com.codeborne.selenide.Selenide;
-import org.junit.jupiter.api.DisplayName;
-import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
 import org.junit.jupiter.params.provider.ValueSource;
 
 import static com.codeborne.selenide.Condition.visible;
@@ -26,6 +25,22 @@ public class WebTest {
         //        Ожидаемый результат:
         $$(".serp-item")
                 .find(Condition.text(testData))
+                .shouldBe(visible);
+    }
+    @CsvSource(value = {
+            "Selenide, is an open source library for test",
+            "JUnit, Support JUnit"
+    })
+    @ParameterizedTest(name = "Проверка поиска в яндексе по слову {0}, ожидаем результат {1}")
+    void yaSearchComplexTest(String testData, String expectedResult) {
+        //        Предусловия:
+        Selenide.open("https://ya.ru");
+        //        Шаги:
+        $("#text").setValue(testData);
+        $("button[type='submit']").click();
+        //        Ожидаемый результат:
+        $$(".serp-item")
+                .find(Condition.text(expectedResult))
                 .shouldBe(visible);
     }
 }
