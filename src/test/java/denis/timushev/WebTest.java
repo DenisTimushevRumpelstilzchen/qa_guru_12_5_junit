b1753baf28b9bde138570d6a7348287b826702b3
Your NameDenis <tdv14011984@gmail.com>
2022-04-26 12:54:43 +0300

    add parameterized test

--- src/test/java/denis/timushev/WebTest.java
+++ src/test/java/denis/timushev/WebTest.java
@@ -4,37 +4,28 @@ import com.codeborne.selenide.Condition;
 import com.codeborne.selenide.Selenide;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
 
 import static com.codeborne.selenide.Condition.visible;
 import static com.codeborne.selenide.Selenide.$;
 import static com.codeborne.selenide.Selenide.$$;
 
 public class WebTest {
-    @DisplayName("Проверка поиска в яндексе по слову Selenide")
-    @Test
-    void selenideSearchTest() {
+    @ValueSource(strings = {
+            "Selenide",
+            "JUnit"
+    })
+    @ParameterizedTest(name = "Проверка поиска в яндексе по слову {0}")
+    void yaSearchTest(String testData) {
         //        Предусловия:
         Selenide.open("https://ya.ru");
         //        Шаги:
-        $("#text").setValue("Selenide");
+        $("#text").setValue(testData);
         $("button[type='submit']").click();
         //        Ожидаемый результат:
         $$(".serp-item")
-                .find(Condition.text("Selenide"))
-                .shouldBe(visible);
-    }
-
-    @DisplayName("Проверка поиска в яндексе по слову JUnit")
-    @Test
-    void jUnitSearchTest() {
-        //        Предусловия:
-        Selenide.open("https://ya.ru");
-        //        Шаги:
-        $("#text").setValue("JUnit");
-        $("button[type='submit']").click();
-        //        Ожидаемый результат:
-        $$(".serp-item")
-                .find(Condition.text("JUnit"))
+                .find(Condition.text(testData))
                 .shouldBe(visible);
     }
 }
